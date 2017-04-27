4d6b6425e43b01dfdb6364a66e7ffc25f5635206
Ben-Hu <Benjqh@gmail.com>
2017-04-27 18:23:37 -0400

    wat?

--- 2.py
+++ 2.py
@@ -1,4 +1,13 @@
+#lol
 def pick_peaks(arr):
     pos = list(filter(lambda x: (arr[x] >= arr[x-1]) and (arr[x] >= arr[x+1]) and (arr[x] != arr[x-1]) and (set([arr[x]]) | set(arr[x:]) != set([arr[x]])) and (arr[x] > [arr[n] for n in range(x,len(arr)) if arr[n] != arr[x]][0]), range(1,len(arr)-1)))
     peaks = list(map(arr.__getitem__, pos))
     return {'pos': pos, 'peaks': peaks}
+
+#function chaining
+def add(n):
+    return wat(n)
+    
+class wat(int):
+    def __call__(self, n):
+        return wat(self + n)
