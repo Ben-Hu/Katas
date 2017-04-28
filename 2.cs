f699289a9949930391cedb7fefdec716727257ee
Ben-Hu <Benjqh@gmail.com>
2017-04-28 15:28:24 -0400

    clean

--- 2.cs
+++ 2.cs
@@ -1,27 +1,18 @@
-using System.Linq;
+using System;
 
 public class Xbonacci
 {
   public double[] Tribonacci(double[] signature, int n)
   {
     //Lets iterate
-    if ( n == 0 )
+    double[] seq = new double[n];
+    Array.Copy(signature, seq, Math.Min(3, n));
+    
+    for (int i = 3; i < n; i++)
     {
-      return new double[1] {0};
-    } else if ( n <= 3 )
-    {
-      return signature.Take(n).ToArray();
-    } else
-    {
-      double[] seq = new double[n];
-      seq[0] = signature[0];
-      seq[1] = signature[1];
-      seq[2] = signature[2];
-      for (int i = 3; i < n; i++) 
-      {
-        seq[i] = seq[i-1] + seq[i-2] + seq[i-3];
-      }
-      return seq;
+      seq[i] = seq[i-1] + seq[i-2] + seq[i-3];
     }
+
+    return n == 0 ? new double[]{0} : seq;
   }
 }
